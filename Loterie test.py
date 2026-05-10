63947c98204e171bb1e07ea3688d015d1b721d20
seb67760 <146946626+seb67760@users.noreply.github.com>
2026-05-10 19:01:39 +0200

    Update Loterie test.py

--- Loterie test.py
+++ Loterie test.py
@@ -15,7 +15,7 @@ st.title("🎯 Analyse Loto")
 
 @st.cache_data
 
-'url = "https://loto.akroweb.fr/loto-historique-tirages"
+# url = "https://loto.akroweb.fr/loto-historique-tirages"
 html = requests.get("https://loto.akroweb.fr/loto-historique-tirages").content
 df = pd.read_html(html)[0][[2,4,5,6,7,8,9]]
 
